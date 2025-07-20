4cf8d44c4f9dac9137b962c5527c62b6b4be925e
Thekra Qaqish <qaqishth@gmail.com>
2025-07-20 21:40:26 +0300

    adding the solution of the delete middle element from stack

---
+++ Data-Structure/StackAndQueue/stackAndQueue/deleteMiddleElement/deleteMiddlaeElement.js
@@ -0,0 +1,32 @@
+const Stack = require('../stack');
+class stackWithDeleteMiddle extends Stack {
+
+    deleteMiddle() {
+        let length = 0;
+        let current = this.top;
+        while (current) {
+            length++;
+            current = current.next;
+        }
+
+        const middle = Math.floor(length / 2);
+
+        const tempStack = new Stack();
+
+        for (let i = 0; i < length; i++) {
+            let val = this.pop();
+            if (i !== middle) {
+                tempStack.push(val);
+            }
+        }
+
+        while (!tempStack.isEmpty()) {
+            this.push(tempStack.pop());
+        }
+
+        return this;
+    }
+
+}
+
+module.exports=stackWithDeleteMiddle;
